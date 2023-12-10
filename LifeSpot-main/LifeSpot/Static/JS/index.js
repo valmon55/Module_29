4f2ae41bbfca4f589b16d32e3171af612a558e40
valmon55 <fedor_ka@mail.ru>
2023-12-10 21:14:35 +0300

    func parm

--- LifeSpot-main/LifeSpot/Static/JS/index.js
+++ LifeSpot-main/LifeSpot/Static/JS/index.js
@@ -26,8 +26,9 @@ let checkAge = function CheckAge() {
 		console.log(result)
 	}
 }
-let filterVideos = function FilterVideos() {
-	let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
+//let filterVideos =
+function FilterVideos(inputString) {
+	//let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
 	let elements = document.getElementsByClassName('video-container');
 	for (let i = 0; i < elements.length; i++) {
 		let videoText = elements[i].querySelector('.video-title').innerText;
