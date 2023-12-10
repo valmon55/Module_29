e284c4107e425d71b0273941d4595f8b5e66fc77
valmon55 <fedor_ka@mail.ru>
2023-12-10 21:01:03 +0300

    function Expression

--- LifeSpot-main/LifeSpot/Static/JS/index.js
+++ LifeSpot-main/LifeSpot/Static/JS/index.js
@@ -1,30 +1,32 @@
-// создадим объект Map для хранения сессии
-let session = new Map();
-// Сохраним UserAgent
-session.set("userAgent", window.navigator.userAgent)
+let checkAge = function CheckAge() {
+	// создадим объект Map для хранения сессии
+	let session = new Map();
+	// Сохраним UserAgent
+	session.set("userAgent", window.navigator.userAgent)
 
 
-// Запросим возраст пользователя и тоже сохраним
-session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+	// Запросим возраст пользователя и тоже сохраним
+	session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
 
-// Проверка на возраст и сохранение сессии
-if (session.get("age") >= 18) {
-    let startDate = new Date().toLocaleString();
+	// Проверка на возраст и сохранение сессии
+	if (session.get("age") >= 18) {
+		let startDate = new Date().toLocaleString();
 
-    alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
-    session.set("startDate", startDate)
+		alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
+		session.set("startDate", startDate)
+	}
+	else {
+		alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+		window.location.href = "http://www.google.com"
+		a = true + 20 + "name"
+	}
+
+	// Вывод в консоль
+	for (let result of session) {
+		console.log(result)
+	}
 }
-else {
-    alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-    window.location.href = "http://www.google.com"
-    a = true + 20 + "name"
-}
-
-// Вывод в консоль
-for (let result of session) {
-    console.log(result)
-}
-function FilterVideos() {
+let filterVideos = function FilterVideos() {
 	let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
 	let elements = document.getElementsByClassName('video-container');
 	for (let i = 0; i < elements.length; i++) {
