88336d2e41ce2f247f032f49cf5212eb126aed66
valmon55 <fedor_ka@mail.ru>
2023-12-14 16:11:23 +0300

    29.7.5 part 3

--- LifeSpot-main/LifeSpot/Static/JS/about.js
+++ LifeSpot-main/LifeSpot/Static/JS/about.js
@@ -1,2 +1,19 @@
-    alert("about.js работает!");
+//alert("about.js работает!");
+function getFeedbackData()
+{
+    let feedback = {};
+    feedback["name"] = prompt("Введите ваше имя:");
+    if (feedback["name"] == null) { return };
+    feedback["comment"] = prompt("Введите ваш комментарий");
+    feedback["date"] = new Date().toLocaleString();
+
+    addFeedback(feedback);
+}
+const addFeedback = feedback => 
+{
+    let elem = document.getElementsByClassName("review-button")[0];
+    elem.innerHTML += `<p>${feedback['name']}<p>` +
+        `<p>${feedback['date']}<p>` +
+        `<p>${feedback['comment']}<p>`;
+}
 
